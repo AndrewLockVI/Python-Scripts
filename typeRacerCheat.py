318133801cc0ebab00d4acc25c73adffa358566a
CodexGameDev <andrewlaack1@gmail.com>
2023-03-19 13:29:04 -0500

    Refactored

--- typeRacerCheat.py
+++
@@ -1,7 +0,0 @@
-import requests
-import selenium.webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
