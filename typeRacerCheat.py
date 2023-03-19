46d8c8ff29ede8b911fbc75285982012b58d1b23
CodexGameDev <andrewlaack1@gmail.com>
2023-03-19 13:03:51 -0500

    Tried creating automation to run snake

---
+++ typeRacerCheat.py
@@ -0,0 +1,7 @@
+import requests
+import selenium.webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
