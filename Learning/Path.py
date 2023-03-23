0393852809ac0a409c1ed3288c39f74a6f1b1fc4
CodexGameDev <andrewlaack1@gmail.com>
2023-03-22 20:51:09 -0500

    Playing more with paths.

--- Learning/Path.py
+++ Learning/Path.py
@@ -9,17 +9,19 @@ from pathlib import Path
 #__file__ is the refrence to where this file is at.
 dir_name = os.path.dirname(__file__)
 
+
 #Changes the current working directory to that of the parent of the current file
 os.chdir(dir_name)
+print(dir_name)
 
-#Prints the current working path
-print(Path.cwd())
-i = 0
-#Adds in some blank lines
-while i < 5:
-    print('')
-    i += 1
+# #Prints the current working path
+# print(Path.cwd())
+# i = 0
+# #Adds in some blank lines
+# while i < 5:
+#     print('')
+#     i += 1
 
 
-#Print out directory listing
-print(os.popen('dir').read())
+# #Print out directory listing
+# print(os.popen('dir').read())
