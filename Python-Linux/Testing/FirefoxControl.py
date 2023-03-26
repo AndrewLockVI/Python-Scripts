f0b64e90591b197b37879de12e4518c68bc1c1ad
Andrew Laack <andrewlaack1@gmail.com>
2023-03-25 22:27:05 -0500

    Tested controlling firefox using python on linux.

---
+++ Python-Linux/Testing/FirefoxControl.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+import time
+driver = webdriver.Firefox()
+driver.get("https://www.google.com")
+
+driver.find_element(by=By.CLASS_NAME, value='gLFyf').send_keys('testing')
+time.sleep(3)
+driver.close()
+
+
