c6b5c344e0ddf12684f05ac5f1be07c0ce923f5b
Andrew Laack <andrewlaack1@gmail.com>
2023-03-28 20:57:05 -0500

    Refactoring

---
+++ python-linux/practicing/sleep_sort.py
@@ -0,0 +1,20 @@
+import threading
+import time
+import importlib
+import sort_algorithm_comparison as sac
+
+def sleep_sort(input_num):
+    input_num = int(input_num)
+    time.sleep(input_num)
+    print(input_num)
+
+
+
+
+
+if __name__ == '__main__':
+    long_list = sac.random_list(10)
+    print(long_list) 
+    for i in long_list:
+        threading.Thread(target = sleep_sort, args=(i,)).start()
+     
--- python-linux/practicing/sort_algorithm_comparison.py
+++ python-linux/practicing/sort_algorithm_comparison.py
@@ -3,9 +3,52 @@ import random
 import time
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+def selection_sort(tuple_to_sort):
+    smallest_val = tuple_to_sort[0]
+    sort_list = list(tuple_to_sort)
+    sorted_list = []
+    smallest_index = 0
+    length = len(sort_list)
+    x = 0
+    while x < length - 1:
+        count = 0
+        for i in sort_list:
+            if i < smallest_val:
+                smallest_val = i
+                smallest_index = count
+            count += 1
+        sort_list.pop(smallest_index)
+        sorted_list.append(smallest_val)
+        x += 1
+        smallest_val = sort_list[0]
+        smallest_index = 0
+
+
+
+    #This adds on the last value because it can't be compared to any other
+    #Values it would make the above function error out.
+    sorted_list.append(sort_list[0])
+    return sorted_list
+
+
+
+
 def random_list(length):
     count = 0
-    while count < list_length:
+    list_to_sort = []
+    while count < length:
         count += 1
         list_to_sort.append(int(100 * random.random()))
     return list_to_sort
@@ -15,7 +58,6 @@ def random_list_change(sort_list):
     sorted_list = []
     length = len(sort_list)
     sort_list = list(sort_list)
-    
 
     while len(sort_list) > 0:
         rnd_val = random.randint(0, len(sort_list) - 1)
@@ -85,34 +127,39 @@ if __name__ == '__main__':
         list_length = int(sys.argv[1])
     except:
         list_length = int(input("How long would you like the list to be: "))
+    try:
+        sort = sys.argv[2]
+    except:
+        sort = 'all'
+
     list_to_sort = []
     list_to_sort = random_list(list_length)
     
     #This times how long the count sort takes
     
-    t0 = time.time()
-    sorted_list = count_sort(list_to_sort)
-    t1 = time.time()
-    count_time = t1 - t0
+    if sort == 'count' or sort == 'all':
+        t0 = time.time()
+        sorted_list = count_sort(list_to_sort)
+        t1 = time.time()
+        count_time = t1 - t0
+        print("Count Sort Time: " + str(count_time))
 
-    t0 = time.time()
-    sorted_list = random_sort(tuple(list_to_sort))
-    t1 = time.time()
-    random_time = t1 - t0
+    if sort == 'random' or sort == 'all':
+        t0 = time.time()
+        sorted_list = random_sort(tuple(list_to_sort))
+        t1 = time.time()
+        random_time = t1 - t0
+        print("Random Sort Time: " + str(random_time))
+
+    if sort == 'selection' or sort == 'all':
+        t0 = time.time()
+        sorted_list = selection_sort(tuple(list_to_sort))
+        t1 = time.time()
+        selection_time = t1 - t0
+        print("Selection Sort Time: " + str(selection_time))
     
-    print("Random Sort Time: " + str(random_time))
-    print("Count Sort Time: " + str(count_time))
-    print(sorted_list)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    try:
+        print(sorted_list)
+    except:
+        print('Invalid option please input an integer then a sort type. Leave type blank for all.')
